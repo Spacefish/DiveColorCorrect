9f4f13fc0cdea595f7b9846a1a986f2cc77c493f
Timo Witte <timo.witte@gmail.com>
2018-10-08 22:22:47 +0200

    add divewb

---
+++ DiveColorCorrect/DiveWB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCvSharp;
+
+namespace DiveColorCorrect
+{
+    public class DiveWB : OpenCvSharp.XPhoto.WhiteBalancer
+    {
+        public override void BalanceWhite(InputArray src, OutputArray dst)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
